fb14250de150e4bc2532d6f42022a166b27c8688
Sheng Chou <chousheng@gmail.com>
2023-02-04 21:08:20 -0800

    RTIOW: 4. Rays, a Simple Camera, and Background (Listing 9)
    - Image 2: A blue-to-white gradient depending on ray Y coordinate
    - Implement Copy for Vec3
    - Add ray.rs
    - Overload arithmetic operators for Vec3
    - Fix problems reported by Clippy

--- src/color.rs
+++ src/color.rs
@@ -1,10 +1,10 @@
 use std::io::Write;
 
-use crate::vec3::Color3;
+use crate::vec3::Color;
 
-pub fn write_color<T: Write>(write: &mut T, pixel_color: &Color3) {
+pub fn write_color<T: Write>(write: &mut T, pixel_color: Color) {
     let r = (255.999 * pixel_color.x()) as i32;
     let g = (255.999 * pixel_color.y()) as i32;
     let b = (255.999 * pixel_color.z()) as i32;
-    write!(write, "{} {} {}\n", r, g, b).expect("writing color");
+    writeln!(write, "{} {} {}", r, g, b).expect("writing color");
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,24 +1,79 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+#[derive(Clone, Copy)]
 pub struct Vec3 {
-    data: [f64; 3],
+    e: [f64; 3],
 }
 
 impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
-        Vec3 { data: [x, y, z] }
+        Vec3 { e: [x, y, z] }
     }
 
     pub fn x(&self) -> f64 {
-        self.data[0]
+        self.e[0]
     }
 
     pub fn y(&self) -> f64 {
-        self.data[1]
+        self.e[1]
     }
 
     pub fn z(&self) -> f64 {
-        self.data[2]
+        self.e[2]
+    }
+
+    pub fn length(&self) -> f64 {
+        f64::sqrt(self.length_squared())
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
 }
 
-//pub type Point3 = Vec3;
-pub type Color3 = Vec3;
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(self * rhs.x(), self * rhs.y(), self * rhs.z())
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3::new(self.x() * rhs, self.y() * rhs, self.z() * rhs)
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec3::new(self.x() / rhs, self.y() / rhs, self.z() / rhs)
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z())
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z())
+    }
+}
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+
+pub fn unit_vector(v: Vec3) -> Vec3 {
+    v / v.length()
+}
