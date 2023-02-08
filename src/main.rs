511fd523ab1afa274e05979e5bcd50f88ae118ee
Sheng Chou <chousheng@gmail.com>
2023-02-07 16:21:39 -0800

    RTIOW: 8. Diffuse Materials (Listing 38)
    - Image 9: Correct rendering of Lambertian spheres

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     }
 
     if world.hit(r, 0.001, f64::INFINITY, &mut rec) {
-        let target = rec.p + rec.normal + vec3::random_in_unit_sphere();
+        let target = rec.p + rec.normal + vec3::random_unit_vector();
         return 0.5 * ray_color(&Ray::new(rec.p, target - rec.p), world, depth - 1);
     }
 
