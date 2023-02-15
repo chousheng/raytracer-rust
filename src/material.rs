9c093077d1a10b3d33a153ead46028e4c39eed4a
Sheng Chou <chousheng@gmail.com>
2023-02-15 11:11:21 -0800

    RTIOW: 10. Dielectrics (Listing 61)
    - Image 16: A hollow glass sphere
    - Schlick Approximation

--- src/material.rs
+++ src/material.rs
@@ -1,5 +1,6 @@
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
+use crate::rtweekend;
 use crate::vec3;
 use crate::vec3::Color;
 use crate::vec3::Vec3;
@@ -85,6 +86,13 @@ impl Dielectric {
             ir: index_of_refraction,
         }
     }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+        r0 = r0 * r0;
+        r0 + (1.0 - r0) * f64::powf(1.0 - cosine, 5.0)
+    }
 }
 
 impl Material for Dielectric {
@@ -107,12 +115,14 @@ impl Material for Dielectric {
         let sin_theta = f64::sqrt(1.0 - cos_theta * cos_theta);
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
+        let direction = if cannot_refract
+            || Self::reflectance(cos_theta, refraction_ratio) > rtweekend::random_double()
+        {
             vec3::reflect(unit_direction, rec.normal)
         } else {
             vec3::refract(unit_direction, rec.normal, refraction_ratio)
         };
-        
+
         *scattered = Ray::new(rec.p, direction);
         true
     }
